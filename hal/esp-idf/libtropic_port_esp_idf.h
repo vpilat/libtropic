54fdf8001156ee5c7d896545e418fce357179a96
Jan Medek <czmedexs@gmail.com>
2025-12-08 17:00:45 +0100

    refactor(lt_dev_esp_idf_t): reorder, use gpio_num_t for pin variables

--- hal/esp-idf/libtropic_port_esp_idf.h
+++ hal/esp-idf/libtropic_port_esp_idf.h
@@ -21,24 +21,26 @@
  *       libtropic.
  */
 typedef struct lt_dev_esp_idf_t {
-    /** @brief @public GPIO pin used for chip select. */
-    gpio_num_t spi_cs_gpio_pin;
     /** @brief @public SPI host peripheral ID. */
     spi_host_device_t spi_host_id;
+    /** @brief @public GPIO pin used for chip select. */
+    gpio_num_t spi_cs_gpio_pin;
     /** @brief @public SPI MISO pin. */
-    int spi_miso_pin;
+    gpio_num_t spi_miso_pin;
     /** @brief @public SPI MOSI pin. */
-    int spi_mosi_pin;
+    gpio_num_t spi_mosi_pin;
     /** @brief @public SPI CLK pin. */
-    int spi_clk_pin;
+    gpio_num_t spi_clk_pin;
     /** @brief @public SPI CLK frequency (Hz). */
     int spi_clk_hz;
+#if LT_USE_INT_PIN
+    /** @brief @public GPIO pin connected to TROPIC01's interrupt pin. */
+    gpio_num_t int_gpio_pin;
+#endif
 
     /** @brief @private SPI handle. */
     spi_device_handle_t spi_handle;
 #if LT_USE_INT_PIN
-    /** @brief @public GPIO pin connected to TROPIC01's interrupt pin. */
-    gpio_num_t int_gpio_pin;
     /** @brief @private Semaphore for the TROPIC01's interrupt pin. */
     SemaphoreHandle_t int_gpio_sem;
 #endif
