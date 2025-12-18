aaa54147da226408b889fd2b97f36d34d268b201
andreondra <golasowski.o@gmail.com>
2025-12-18 15:37:04 +0100

    refactor(l432kc): reflect Libtropic changes

--- hal/stm32/nucleo_l432kc/libtropic_port_stm32_nucleo_l432kc.c
+++ hal/stm32/nucleo_l432kc/libtropic_port_stm32_nucleo_l432kc.c
@@ -1,9 +1,12 @@
 /**
  * @file libtropic_port_stm32_nucleo_l432kc.c
  * @copyright Copyright (c) 2020-2025 Tropic Square s.r.o.
- *
+ * @brief Port for STM32 L432KC using native SPI HAL (and GPIO HAL for chip select).
+ * 
  * @license For the license see LICENSE.md in the root directory of this source tree.
  */
+
+#include "libtropic_port_stm32_nucleo_l432kc.h"
 
 #include <stdarg.h>
 #include <stdint.h>
@@ -15,28 +18,21 @@
 #include "libtropic_port.h"
 #include "stm32l4xx_hal.h"
 
-#if LT_USE_INT_PIN
-#error "Interrupt PIN support on NUCLEO-L432KC not implemented yet!"
-#endif
-
-// CS pin
-#define LT_SPI_CS_BANK GPIOA
-#define LT_SPI_CS_PIN GPIO_PIN_4
-// Spi instance
-#define LT_SPI_INSTANCE SPI1
-
-// Random number generator's handle
-RNG_HandleTypeDef rng;
-// SPI handle declaration
-SPI_HandleTypeDef SpiHandle;
-
 lt_ret_t lt_port_random_bytes(lt_l2_state_t *s2, void *buff, size_t count)
 {
-    LT_UNUSED(s2);
+    lt_dev_stm32_nucleo_l432kc_t *device = (lt_dev_stm32_nucleo_l432kc_t *)(s2->device);
     size_t bytes_left = count;
     uint8_t *buff_ptr = buff;
+    int ret;
+    uint32_t random_data;
+
     while (bytes_left) {
-        uint32_t random_data = HAL_RNG_GetRandomNumber(&rng);
+        ret = HAL_RNG_GenerateRandomNumber(device->rng_handle, &random_data);
+        if (ret != HAL_OK) {
+            LT_LOG_ERROR("HAL_RNG_GenerateRandomNumber failed, ret=%d", ret);
+            return LT_FAIL;
+        }
+
         size_t cpy_cnt = bytes_left < sizeof(random_data) ? bytes_left : sizeof(random_data);
         memcpy(buff_ptr, &random_data, cpy_cnt);
         bytes_left -= cpy_cnt;
@@ -46,95 +42,108 @@ lt_ret_t lt_port_random_bytes(lt_l2_state_t *s2, void *buff, size_t count)
     return LT_OK;
 }
 
+
 lt_ret_t lt_port_spi_csn_low(lt_l2_state_t *h)
 {
-    LT_UNUSED(h);
+    lt_dev_nucleo_l432kc_t *device = (lt_dev_nucleo_l432kc_t *)(h->device);
 
-    HAL_GPIO_WritePin(LT_SPI_CS_BANK, LT_SPI_CS_PIN, GPIO_PIN_RESET);
-    while (HAL_GPIO_ReadPin(LT_SPI_CS_BANK, LT_SPI_CS_PIN)) {
-        ;
-    }
+    HAL_GPIO_WritePin(device->spi_cs_gpio_bank, device->spi_cs_gpio_pin, GPIO_PIN_RESET);
+    while (HAL_GPIO_ReadPin(device->spi_cs_gpio_bank, device->spi_cs_gpio_pin));
 
     return LT_OK;
 }
 
 lt_ret_t lt_port_spi_csn_high(lt_l2_state_t *h)
 {
-    LT_UNUSED(h);
+    lt_dev_nucleo_l432kc_t *device = (lt_dev_nucleo_l432kc_t *)(h->device);
 
-    HAL_GPIO_WritePin(LT_SPI_CS_BANK, LT_SPI_CS_PIN, GPIO_PIN_SET);
-    while (!HAL_GPIO_ReadPin(LT_SPI_CS_BANK, LT_SPI_CS_PIN)) {
-        ;
-    }
+    HAL_GPIO_WritePin(device->spi_cs_gpio_bank, device->spi_cs_gpio_pin, GPIO_PIN_SET);
+    while (!HAL_GPIO_ReadPin(device->spi_cs_gpio_bank, device->spi_cs_gpio_pin));
 
     return LT_OK;
 }
 
 lt_ret_t lt_port_init(lt_l2_state_t *h)
 {
-    LT_UNUSED(h);
+    lt_dev_stm32_nucleo_l432kc_t *device = (lt_dev_stm32_nucleo_l432kc_t *)(h->device);
+    int ret;
 
-    // RNG
-    rng.Instance = RNG;
+    // Set the SPI parameters.
+    device->spi_handle.Instance = device->spi_instance;
 
-    if (HAL_RNG_DeInit(&rng) != HAL_OK) {
-        return LT_FAIL;
+    if (device->baudrate_prescaler == 0) {
+        device->spi_handle.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32;
+    }
+    else {
+        device->spi_handle.Init.BaudRatePrescaler = device->baudrate_prescaler;
     }
 
-    if (HAL_RNG_Init(&rng) != HAL_OK) {
-        return LT_FAIL;
+    device->spi_handle.Init.Direction = SPI_DIRECTION_2LINES;
+    device->spi_handle.Init.CLKPhase = SPI_PHASE_1EDGE;
+    device->spi_handle.Init.CLKPolarity = SPI_POLARITY_LOW;
+    device->spi_handle.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+    device->spi_handle.Init.DataSize = SPI_DATASIZE_8BIT;
+    device->spi_handle.Init.FirstBit = SPI_FIRSTBIT_MSB;
+    device->spi_handle.Init.NSS = SPI_NSS_HARD_OUTPUT;
+    device->spi_handle.Init.TIMode = SPI_TIMODE_DISABLE;
+    device->spi_handle.Init.Mode = SPI_MODE_MASTER;
+
+    ret = HAL_SPI_Init(&device->spi_handle);
+    if (ret != HAL_OK) {
+        LT_LOG_ERROR("Failed to init SPI, ret=%d", ret);
+        return LT_L1_SPI_ERROR;
     }
 
-    // SPI CS
+    // GPIO for chip select.
     GPIO_InitTypeDef GPIO_InitStruct = {0};
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    HAL_GPIO_WritePin(LT_SPI_CS_BANK, LT_SPI_CS_PIN, GPIO_PIN_SET);
-    GPIO_InitStruct.Pin = LT_SPI_CS_PIN;
+    LT_SPI_CS_CLK_ENABLE();
+    HAL_GPIO_WritePin(device->spi_cs_gpio_bank, device->spi_cs_gpio_pin, GPIO_PIN_SET);
+    GPIO_InitStruct.Pin = device->spi_cs_gpio_pin;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
-    HAL_GPIO_Init(LT_SPI_CS_BANK, &GPIO_InitStruct);
-    // SPI hardware
-    SpiHandle.Instance = LT_SPI_INSTANCE;
-    SpiHandle.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32;
-    SpiHandle.Init.Direction = SPI_DIRECTION_2LINES;
-    SpiHandle.Init.CLKPhase = SPI_PHASE_1EDGE;
-    SpiHandle.Init.CLKPolarity = SPI_POLARITY_LOW;
-    SpiHandle.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-    SpiHandle.Init.DataSize = SPI_DATASIZE_8BIT;
-    SpiHandle.Init.FirstBit = SPI_FIRSTBIT_MSB;
-    SpiHandle.Init.NSS = SPI_NSS_HARD_OUTPUT;
-    SpiHandle.Init.TIMode = SPI_TIMODE_DISABLE;
-    SpiHandle.Init.Mode = SPI_MODE_MASTER;
+    HAL_GPIO_Init(device->spi_cs_gpio_bank, &GPIO_InitStruct);
 
-    if (HAL_SPI_Init(&SpiHandle) != HAL_OK) {
-        return LT_FAIL;
+#if LT_USE_INT_PIN
+    // GPIO for INT pin.
+    LT_INT_CLK_ENABLE();
+    GPIO_InitStruct.Pin = device->int_gpio_pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(device->int_gpio_bank, &GPIO_InitStruct);
+#endif
+
+    return LT_OK;
+}
+
+lt_ret_t lt_port_deinit(lt_l2_state_t *s2)
+{
+    lt_dev_stm32_nucleo_l432kc_t *device = (lt_dev_stm32_nucleo_l432kc_t *)(s2->device);
+    int ret;
+
+    ret = HAL_SPI_DeInit(&device->spi_handle);
+    if (ret != HAL_OK) {
+        LT_LOG_ERROR("Failed to deinit SPI, ret=%d", ret);
+        return LT_L1_SPI_ERROR;
     }
 
     return LT_OK;
 }
 
-lt_ret_t lt_port_deinit(lt_l2_state_t *h)
+lt_ret_t lt_port_spi_transfer(lt_l2_state_t *s2, uint8_t offset, uint16_t tx_data_length, uint32_t timeout_ms)
 {
-    LT_UNUSED(h);
+    lt_dev_stm32_nucleo_l432kc_t *device = (lt_dev_stm32_nucleo_l432kc_t *)(s2->device);
 
-    if (HAL_RNG_DeInit(&rng) != HAL_OK) {
-        return LT_FAIL;
-    }
-
-    HAL_SPI_MspDeInit(&SpiHandle);
-
-    return LT_OK;
-}
-
-lt_ret_t lt_port_spi_transfer(lt_l2_state_t *h, uint8_t offset, uint16_t tx_data_length, uint32_t timeout_ms)
-{
     if (offset + tx_data_length > TR01_L1_LEN_MAX) {
+        LT_LOG_ERROR("Invalid data length!");
         return LT_L1_DATA_LEN_ERROR;
     }
-    int ret = HAL_SPI_TransmitReceive(&SpiHandle, h->buff + offset, h->buff + offset, tx_data_length, timeout_ms);
+    int ret = HAL_SPI_TransmitReceive(&device->spi_handle, s2->buff + offset, s2->buff + offset, tx_data_length,
+                                      timeout_ms);
     if (ret != HAL_OK) {
-        return LT_FAIL;
+        LT_LOG_ERROR("HAL_SPI_TransmitReceive failed, ret=%d", ret);
+        return LT_L1_SPI_ERROR;
     }
 
     return LT_OK;
@@ -148,6 +157,25 @@ lt_ret_t lt_port_delay(lt_l2_state_t *h, uint32_t ms)
 
     return LT_OK;
 }
+
+#if LT_USE_INT_PIN
+lt_ret_t lt_port_delay_on_int(lt_l2_state_t *s2, uint32_t ms)
+{
+    lt_dev_stm32_nucleo_l432kc_t *device = (lt_dev_stm32_nucleo_l432kc_t *)(s2->device);
+    uint32_t time_initial = HAL_GetTick();
+    uint32_t time_actual;
+
+    while ((HAL_GPIO_ReadPin(device->int_gpio_bank, device->int_gpio_pin) == 0)) {
+        time_actual = HAL_GetTick();
+        if ((time_actual - time_initial) > ms) {
+            return LT_L1_INT_TIMEOUT;
+        }
+        // HAL_Delay(ms);
+    }
+
+    return LT_OK;
+}
+#endif
 
 void lt_port_log(const char *format, ...)
 {
---
+++ hal/stm32/nucleo_l432kc/libtropic_port_stm32_nucleo_l432kc.h
@@ -0,0 +1,51 @@
+#ifndef LIBTROPIC_PORT_STM32_NUCLEO_L432KC_H
+#define LIBTROPIC_PORT_STM32_NUCLEO_L432KC_H
+
+/**
+ * @file libtropic_port_stm32_nucleo_l432kc.h
+ * @copyright Copyright (c) 2020-2025 Tropic Square s.r.o.
+ * @brief Port for STM32 L432KC using native SPI HAL (and GPIO HAL for chip select).
+ *
+ * @license For the license see LICENSE.md in the root directory of this source tree.
+ */
+
+#include "libtropic_port.h"
+#include "stm32l4xx_hal.h"
+
+/**
+ * @brief Device structure for STM32 L432KC port.
+ *
+ * @note Public members are meant to be configured by the developer before passing the handle to
+ *       libtropic.
+ */
+typedef struct lt_dev_stm32_nucleo_l432kc_t {
+    /** @brief @public Instance of STM SPI interface. Use STM32 macro (SPIX, e.g. SPI1). */
+    SPI_TypeDef *spi_instance;
+
+    /**
+     * @brief @public Baudrate prescaler value, used to set SPI speed. Use STM32 macro (e.g. SPI_BAUDRATEPRESCALER_32).
+     *
+     * @note If set to zero, it will default to SPI_BAUDRATEPRESCALER_32.
+     */
+    uint16_t baudrate_prescaler;
+
+    /** @brief @public GPIO pin used for chip select. Use STM32 macro (GPIO_PIN_XX). */
+    uint16_t spi_cs_gpio_pin;
+    /** @brief @public GPIO bank of the pin used for chip select. Use STM32 macro (GPIOX). */
+    GPIO_TypeDef *spi_cs_gpio_bank;
+
+#if LT_USE_INT_PIN
+    /** @brief @public GPIO pin used for interrupts. Use STM32 macro (GPIO_PIN_XX). */
+    uint16_t int_gpio_pin;
+    /** @brief @public GPIO bank of the pin used for interrupts. Use STM32 macro (GPIOX). */
+    GPIO_TypeDef *int_gpio_bank;
+#endif
+
+    /** @brief @private Random number generator handle. */
+    RNG_HandleTypeDef *rng_handle;
+
+    /** @brief @private SPI handle. */
+    SPI_HandleTypeDef spi_handle;
+} lt_dev_stm32_nucleo_l432kc_t;
+
+#endif  // LIBTROPIC_PORT_STM32_NUCLEO_L432KC_H
