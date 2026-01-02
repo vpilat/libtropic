b89c83bc928740d8a78f593decc1cdaf9af9261d
Ondrej Golasowski <golasowski.o@gmail.com>
2026-01-02 14:14:36 +0100

    tests(lt_test_mock_hardware_fail): added remaining functions which return HW FAIL.

--- tests/functional_mock/lt_test_mock_hardware_fail.c
+++ tests/functional_mock/lt_test_mock_hardware_fail.c
@@ -47,6 +47,8 @@ int lt_test_mock_hardware_fail(lt_handle_t *h)
     memcpy(kres, kcmd, TR01_AES256_KEY_LEN);
     mock_session_start(h, kcmd, kres);
 
+    // ----------------------------------------------------------------------------------------------------------
+
     LT_LOG_INFO("Mocking HARDWARE_FAIL in Pairing_Key_Write reply...");
     uint8_t dummy_key[4];
     for (int slot = TR01_PAIRING_KEY_SLOT_INDEX_0; slot <= TR01_PAIRING_KEY_SLOT_INDEX_3; slot++){
@@ -64,6 +66,8 @@ int lt_test_mock_hardware_fail(lt_handle_t *h)
         LT_TEST_ASSERT(LT_L3_HARDWARE_FAIL, lt_pairing_key_write(h, dummy_key, slot));
     }
 
+    // ----------------------------------------------------------------------------------------------------------
+
     LT_LOG_INFO("Mocking HARDWARE_FAIL in Pairing_Key_Invalidate reply...");
     for (int slot = TR01_PAIRING_KEY_SLOT_INDEX_0; slot <= TR01_PAIRING_KEY_SLOT_INDEX_3; slot++){
         LT_LOG_INFO("Mocking for slot %d...", slot);
@@ -75,10 +79,55 @@ int lt_test_mock_hardware_fail(lt_handle_t *h)
             TR01_L3_RESULT_HARDWARE_FAIL,
         };
         LT_TEST_ASSERT(LT_OK, mock_l3_result(h, pairing_key_invalidate_plaintext, sizeof(pairing_key_invalidate_plaintext)));
-
-        LT_TEST_ASSERT(LT_OK, lt_random_bytes(h, dummy_key, sizeof(dummy_key)));
         LT_TEST_ASSERT(LT_L3_HARDWARE_FAIL, lt_pairing_key_invalidate(h, slot));
     }
+
+    // ----------------------------------------------------------------------------------------------------------
+
+    LT_LOG_INFO("Mocking HARDWARE_FAIL in R_Config_Write reply...");
+    // Mock replies to the command.
+    LT_TEST_ASSERT(LT_OK, mock_l3_command_responses(h, 1));
+
+    // Mock command result itself.
+    uint8_t r_config_write_plaintext[] = {
+        TR01_L3_RESULT_HARDWARE_FAIL,
+    };
+    LT_TEST_ASSERT(LT_OK, mock_l3_result(h, r_config_write_plaintext, sizeof(r_config_write_plaintext)));
+    LT_TEST_ASSERT(LT_L3_HARDWARE_FAIL, lt_r_config_write(h, TR01_CFG_START_UP_ADDR, 0x00)); // Dummy object
+
+    // ----------------------------------------------------------------------------------------------------------
+
+    LT_LOG_INFO("Mocking HARDWARE_FAIL in I_Config_Write reply...");
+    // Mock replies to the command.
+    LT_TEST_ASSERT(LT_OK, mock_l3_command_responses(h, 1));
+
+    // Mock command result itself.
+    uint8_t i_config_write_plaintext[] = {
+        TR01_L3_RESULT_HARDWARE_FAIL,
+    };
+    LT_TEST_ASSERT(LT_OK, mock_l3_result(h, i_config_write_plaintext, sizeof(i_config_write_plaintext)));
+    LT_TEST_ASSERT(LT_L3_HARDWARE_FAIL, lt_i_config_write(h, TR01_CFG_START_UP_ADDR, 0x00)); // Dummy object
+
+    // ----------------------------------------------------------------------------------------------------------
+
+    LT_LOG_INFO("Mocking HARDWARE_FAIL in R_Mem_Data_Write reply...");
+    // Mock replies to the command.
+    LT_TEST_ASSERT(LT_OK, mock_l3_command_responses(h, 1));
+
+    // Mock command result itself.
+    uint8_t r_mem_data_write_plaintext[] = {
+        TR01_L3_RESULT_HARDWARE_FAIL,
+    };
+    LT_TEST_ASSERT(LT_OK, mock_l3_result(h, r_mem_data_write_plaintext, sizeof(r_mem_data_write_plaintext)));
+
+    uint16_t random_r_mem_slot;
+    LT_TEST_ASSERT(LT_OK, lt_random_bytes(h, &random_r_mem_slot, sizeof(random_r_mem_slot)));
+    random_r_mem_slot %= TR01_R_MEM_DATA_SLOT_MAX + 1;
+    uint8_t random_r_mem_data[16]; // Arbitrary size. Exact number not important, we just need some dummy data.
+    LT_TEST_ASSERT(LT_OK, lt_random_bytes(h, random_r_mem_data, sizeof(random_r_mem_data)));
+    LT_TEST_ASSERT(LT_L3_HARDWARE_FAIL, lt_r_mem_data_write(h, random_r_mem_slot, random_r_mem_data, sizeof(random_r_mem_data)));
+
+    // ----------------------------------------------------------------------------------------------------------
 
     LT_LOG_INFO("Deinitializing handle");
     LT_TEST_ASSERT(LT_OK, lt_deinit((lt_handle_t *)h));
