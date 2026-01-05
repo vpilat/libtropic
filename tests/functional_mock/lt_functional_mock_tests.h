a716c3b4fea5b09a9fc8a308194383a780242f01
Ondrej Golasowski <golasowski.o@gmail.com>
2026-01-05 12:46:39 +0100

    doc(lt_functional_mock_tests): add lt_test_mock_hardware_fail doc

--- tests/functional_mock/lt_functional_mock_tests.h
+++ tests/functional_mock/lt_functional_mock_tests.h
@@ -45,6 +45,20 @@ int lt_test_mock_attrs(lt_handle_t *h);
  */
 int lt_test_mock_invalid_in_crc(lt_handle_t *h);
 
+/**
+ * @brief Test for handling HARDWARE_FAIL return code.
+ * 
+ * Test steps:
+ * 1. Mock Secure Session initialization.
+ * 2. For each of Pairing_Key_Write, Pairing_Key_Invalidate, R_Config_Write, I_Config_Write, R_Mem_Data_Write:
+ *   a. Mock L3 Result with RESULT=HARDWARE_FAIL.
+ *   b. Call Libtropic function corresponding to the L3 Command and verify that Libtropic returns LT_L3_HARDWARE_FAIL. 
+ * 3. Mock Secure Session deinitialization.
+ * 
+ * @param h Handle for communication with TROPIC01
+ * 
+ * @return 0 on success, non-zero on failure.
+ */
 int lt_test_mock_hardware_fail(lt_handle_t *h);
 
 #ifdef __cplusplus
