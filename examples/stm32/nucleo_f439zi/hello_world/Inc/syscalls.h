9ed23c0b2b53324367a52453735264c22d64355d
Ondrej Golasowski <golasowski.o@gmail.com>
2026-01-15 08:04:25 +0100

    feat(examples/stm32/f439zi): initial project.

---
+++ examples/stm32/nucleo_f439zi/hello_world/Inc/syscalls.h
@@ -0,0 +1,39 @@
+#ifndef INC_SYSCALLS_H_
+#define INC_SYSCALLS_H_
+
+#include <stddef.h>
+#include <sys/stat.h>
+#include <sys/times.h>
+
+// Semihosting initialization
+void initialise_monitor_handles(void);
+
+// Syscall prototypes
+void _exit(int status);
+int _close(int file);
+int _execve(char *name, char **argv, char **env);
+int _fork(void);
+int _fstat(int file, struct stat *st);
+int _getpid(void);
+int _isatty(int file);
+int _kill(int pid, int sig);
+int _link(char *old, char *new);
+int _lseek(int file, int ptr, int dir);
+int _open(char *path, int flags, ...);
+int _read(int file, char *ptr, int len);
+int _stat(char *file, struct stat *st);
+struct tms;  // Forward declaration for tms struct
+int _times(struct tms *buf);
+int _unlink(char *name);
+int _wait(int *status);
+int _write(int file, char *ptr, int len);
+// The prototype for _sbrk, used for malloc
+void *_sbrk(ptrdiff_t incr);
+
+// Prototypes for standard I/O retargeting functions.
+// These functions are implemented elsewhere (e.g., main.c)
+// and marked as 'weak' so they can be optionally provided.
+int __io_putchar(int ch);
+int __io_getchar(void);
+
+#endif /* INC_SYSCALLS_H_ */
---
+++ examples/stm32/nucleo_f439zi/hello_world/Src/mbedtls_v4/mbedtls_platform.c
@@ -0,0 +1,43 @@
+#include <stdint.h>
+#include <string.h>
+
+#include "main.h"
+#include "mbedtls/platform.h"
+#include "mbedtls/platform_time.h"
+#include "stm32f4xx_hal.h"
+
+mbedtls_ms_time_t mbedtls_ms_time(void)
+{
+    // Use STM32 HAL millisecond tick. HAL_GetTick() returns uint32_t ms since startup.
+    // See Page 48 of UM1725 Rev 8.
+    return (mbedtls_ms_time_t)HAL_GetTick();
+}
+
+int mbedtls_platform_get_entropy(psa_driver_get_entropy_flags_t flags, size_t *estimate_bits, unsigned char *output,
+                                 size_t output_size)
+{
+    // We don't implement any flags.
+    if (flags != 0) {
+        return PSA_ERROR_NOT_SUPPORTED;
+    }
+
+    HAL_StatusTypeDef hal_status = HAL_OK;
+    uint32_t random_data;
+    size_t bytes_left = output_size;
+
+    while (bytes_left) {
+        hal_status = HAL_RNG_GenerateRandomNumber(&RNGHandle, &random_data);
+        if (hal_status != HAL_OK) {
+            return PSA_ERROR_INSUFFICIENT_ENTROPY;
+        }
+
+        size_t cpy_cnt = bytes_left < sizeof(random_data) ? bytes_left : sizeof(random_data);
+        memcpy(output, &random_data, cpy_cnt);
+        bytes_left -= cpy_cnt;
+        output += cpy_cnt;
+    }
+
+    *estimate_bits = 8 * output_size;
+
+    return 0;
+}
